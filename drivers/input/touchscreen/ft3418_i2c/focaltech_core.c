485460eb7c665292baa78421534730d3334032df
Cyber Knight <cyberknight755@gmail.com>
2023-02-10 14:37:23 +0800

    Revert "touchscreen: ft3418_i2c: Use msm drm notifier API"
    This reverts commit c99d59e4597144319c81dd5ab2b98ebd3a7169ef.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/input/touchscreen/ft3418_i2c/focaltech_core.c
+++ drivers/input/touchscreen/ft3418_i2c/focaltech_core.c
@@ -1242,8 +1242,8 @@ static int drm_notifier_callback(struct notifier_block *self,
         return 0;
     }
 
-    if (!((event == MSM_DRM_EARLY_EVENT_BLANK )
-          || (event == MSM_DRM_EVENT_BLANK))) {
+    if (!((event == DRM_EARLY_EVENT_BLANK )
+          || (event == DRM_EVENT_BLANK))) {
         FTS_INFO("event(%lu) do not need process\n", event);
         return 0;
     }
@@ -1251,18 +1251,18 @@ static int drm_notifier_callback(struct notifier_block *self,
     blank = evdata->data;
     FTS_INFO("DRM event:%lu,blank:%d", event, *blank);
     switch (*blank) {
-    case MSM_DRM_BLANK_UNBLANK:
-        if (MSM_DRM_EARLY_EVENT_BLANK == event) {
+    case DRM_BLANK_UNBLANK:
+        if (DRM_EARLY_EVENT_BLANK == event) {
             FTS_INFO("resume: event = %lu, not care\n", event);
-        } else if (MSM_DRM_EVENT_BLANK == event) {
+        } else if (DRM_EVENT_BLANK == event) {
             queue_work(fts_data->ts_workqueue, &fts_data->resume_work);
         }
         break;
-    case MSM_DRM_BLANK_POWERDOWN:
-        if (MSM_DRM_EARLY_EVENT_BLANK == event) {
+    case DRM_BLANK_POWERDOWN:
+        if (DRM_EARLY_EVENT_BLANK == event) {
             cancel_work_sync(&fts_data->resume_work);
             fts_ts_suspend(ts_data->dev);
-        } else if (MSM_DRM_EVENT_BLANK == event) {
+        } else if (DRM_EVENT_BLANK == event) {
             FTS_INFO("suspend: event = %lu, not care\n", event);
         }
         break;
@@ -1417,7 +1417,7 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 	pm_runtime_enable(ts_data->dev);
 
     ts_data->drm_notif.notifier_call = drm_notifier_callback;
-    ret = msm_drm_register_client(&ts_data->drm_notif);
+    ret = drm_register_client(&ts_data->drm_notif);
     if (ret) {
         FTS_ERROR("Unable to register drm_notifier: %d\n", ret);
     }
@@ -1511,7 +1511,7 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
     if (ts_data->ts_workqueue)
         destroy_workqueue(ts_data->ts_workqueue);
 
-    if (msm_drm_unregister_client(&ts_data->drm_notif))
+    if (drm_unregister_client(&ts_data->drm_notif))
         FTS_ERROR("Error occurred while unregistering drm_notifier.\n");
 
     if (gpio_is_valid(ts_data->pdata->reset_gpio))
